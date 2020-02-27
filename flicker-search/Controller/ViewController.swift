19b9bec4c6a2a1ac20785945a74522b8afcc1314
Gabriel <gabriel.sgaspar@yahoo.com.br>
2020-02-27 17:41:57 -0500

    Adicionando MARKs e removendo comentarios desnecessarios do ViewController

--- flicker-search/Controller/ViewController.swift
+++ flicker-search/Controller/ViewController.swift
@@ -91,42 +91,10 @@ class ViewController: UIViewController {
 }
 
 
+// MARK: - CollectionView Delegate and DataSource Protocols
+
 extension ViewController: UICollectionViewDelegateFlowLayout {
 
-    // Define o tamanho de um item em uma dada celula
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        print("Fui chamado!")
-//        let size = CGSize(width: collectionViewLayout.collectionViewContentSize.width, height: collectionViewLayout.collectionViewContentSize.height)
-//
-//        return size
-//    }
-    
-    
-    // Definir as margens para serem aplicadas ao conteudo de uma determinada secao
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-//
-//    }
-    
-    // Definir o espacamento entre linhas ou colunas adjacentes de uma secao
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-//        <#code#>
-//    }
-    
-    // Definir o espacamento entre itens sucessivos de uma secao. (seja em uma coluna ou linha)
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-//        <#code#>
-//    }
-    
-    // Definir o tamanho do header em uma dada secao
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-//        <#code#>
-//    }
-
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        //print(scrollView.contentOffset.x)
-//        let offset = scrollView.contentOffset.x / CGFloat(4.0)
-//        menuBar?.moveHorizontalBar(toOffset: offset)
-    }
     func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
 //        print("Will end dragging")
 //        print(targetContentOffset.pointee.x)
@@ -135,8 +103,6 @@ extension ViewController: UICollectionViewDelegateFlowLayout {
         let indexPath = IndexPath(item: index, section: 0)
         menuBar?.selectItem(atIndexPath: indexPath)
     }
-    
-
 }
 
 extension ViewController: UICollectionViewDataSource {
@@ -172,6 +138,7 @@ extension ViewController: UICollectionViewDataSource {
     }
 }
 
+// MARK: - Settings Launcher Protocol
 extension ViewController: SettingsLauncherProtocol {
     func didSelectSettingItem(item: K.SettingItems) {
         if item == K.SettingItems.Settings {
@@ -181,6 +148,7 @@ extension ViewController: SettingsLauncherProtocol {
     }
 }
 
+// MARK: - MenuBarDelegate Protocol
 extension ViewController: MenuBarDelegate {
     func didSelectMenuBarItem(atIndexPath indexPath: IndexPath) {
         flickerView?.collectionView.scrollToItem(at: indexPath, at: .left, animated: true)
